ea78dbe3d9e1d2091291d3e31d0b12e0fb02c7d6
DearSjh <95723515@qq.com>
2019-03-16 22:00:56 +0800

    2.0.00版本

--- pages/detail/detail.js
+++ pages/detail/detail.js
@@ -10,24 +10,63 @@ Page({
    * 页面的初始数据
    */
   data: {
+    percent:0,
+    duration:'',
+    zhuanma:'running',
+    currentTime:'',
+    i:2,
+    pic:'',
    song:[],
+   songName:'',
     playStatu:'stop.png',
     back:'',
+    songList:[],
    play:false,
     songError:'',
     audioAction: {
       method: 'start'
     }
   },
+  lastSong: function () {
+    var i = Math.floor((Math.random() * this.data.songList.length));
+    this.setData({
+      song: this.data.songList[i]
+    })
+    const last = wx.getBackgroundAudioManager();
+    last.src = this.data.songList[i].url;
+    last.title = this.data.songList[i].name;
+    last.coverImgUrl = this.data.songList[i].pic;
+    // console.log(this.data.songList[this.data.i]);
+    // next.play();
+  },
+  nextSong:function(){
+    var i = Math.floor((Math.random() * this.data.songList.length));
+    const next = wx.getBackgroundAudioManager();
+    this.setData({
+      song: this.data.songList[i]
+    })
+    next.src = this.data.songList[i].url;
+    next.title = this.data.songList[i].name;
+    next.coverImgUrl = this.data.songList[i].pic;
+    // console.log(this.data.songList[this.data.i]);
+    // next.play(function () {
+    //   this.setData({
+    //     pic: this.data.songList[i].pic
+    //   })
+    //   console.log(this.data.pic);
+    // });
+  },
   stopPlay:function(){
     if(this.data.playStatu =='stop.png'){
       this.setData({
-        playStatu: 'play.png'
+        playStatu: 'play.png',
+        zhuanma:'paused'
       })
       wx.getBackgroundAudioManager().pause();
     }else{
       this.setData({
-        playStatu: 'stop.png'
+        playStatu: 'stop.png',
+        zhuanma:'running'
       })
       wx.getBackgroundAudioManager().play();
     }
@@ -36,17 +75,75 @@ Page({
   onLoad: function (options) {
     // 页面初始化 options为页面跳转所带来的参数
     var that = this;
+    wx.getBackgroundAudioPlayerState({
+      success(res) {
+        var status = res.status
+        const dataUrls = res.dataUrl.indexOf('?');
+        const dataUrle = res.dataUrl.indexOf('&');
+        var id = (status == 2?'28285910':res.dataUrl.slice(dataUrls+4,dataUrle));
+        var currentPosition = res.currentPosition
+        const duration = res.duration
+          wx.request({
+            url:'https://api.bzqll.com/music/netease/song?key=579621905&id='+id,
+            method:'GET',
+            success:function(res){
+              if (res.data.code == 200) { //当歌查询成功
+                let song = res.data.data;
+                let url = res.data.data.url;
+                const back = wx.getBackgroundAudioManager();
+                  back.onTimeUpdate(function () {
+                  var currentTime = back.currentTime;
+                  var percent = Math.floor(100 * currentTime / duration);
+                  that.setData({
+                      currentTime: Math.floor(currentTime),
+                      percent: percent
+                    })
+                  })
+                that.setData({
+                  song: song,
+                  songName:song.name,
+                  pic: song.pic
+                  
+                })
+                    wx.request({
+                      url: 'https://api.bzqll.com/music/netease/search?key=579621905&type=song&limit=100&offset=0&s=' + that.data.song.name,
+                      success: res => {
+                        // console.log(res.data.code);
+                        console.log(res.data);
+                        if (res.data.code == 200) {
+                          let searchEnd = res.data.data;
+                          // console.log(options.songName);
+                          that.setData({
+                            songList: res.data.data,
+                          })
+                          // console.log(this.data.songList[0]);
+                        } else {
+                          taht.setData({
+                            songList: [],
+                          })
+                        }
+                      }
+                    })
+              }
+            }
+          })
+          
+
+      }
+    })
     wx.request({
+      // url:'https://api.bzqll.com/music/netease/song?key=579621905&id=526307800',
       url: 'https://api.bzqll.com/music/netease/song?key=579621905&id=' + options.id,
       method: 'GET', // OPTIONS, GET, HEAD, POST, PUT, DELETE, TRACE, CONNECT
       success: function (res) {
         // console.log(res);
-        if (res.data.code == 200) { //当歌词查询成功
+        if (res.data.code == 200) { //当歌查询成功
           let song = res.data.data;
           let url = res.data.data.url;
-          console.log(res.data.data);
+          // console.log(res.data.data);
           that.setData({
-            song:song
+            song:song,
+            pic:song.pic,
           })
           
           const back = wx.getBackgroundAudioManager();
@@ -59,19 +156,70 @@ Page({
               back:back
             })
           });
-          // back.onPlay(() => {
-          //   that.setData({
-          //     playStatu: 'stop.png'
-          //   })
-          // })
-          // back.onPause(()=>{
-          //   that.setData({
-          //     playStatu:'play.png'
-          //   })
-          //   console.log(that.data.playStatu);
-          // })
+          back.onPlay(() => {
+            that.setData({
+              playStatu: 'stop.png',
+              zhuanma:'running',
+              // currentTime:back.currentTime,
+              // duration:back.duration
+            })
+          })
+          //系统面板下一首
+          back.onNext(function(){
+            var i = Math.floor((Math.random() * this.data.songList.length));
+            const next = wx.getBackgroundAudioManager();
+            this.setData({
+              song: this.data.songList[i]
+            })
+            next.src = this.data.songList[i].url;
+            next.title = this.data.songList[i].name;
+            next.coverImgUrl = this.data.songList[i].pic;
+          })
+          //系统面板上一首
+          back.onPrev(function(){
+            var i = Math.floor((Math.random() * this.data.songList.length));
+            this.setData({
+              song: this.data.songList[i]
+            })
+            const last = wx.getBackgroundAudioManager();
+            last.src = this.data.songList[i].url;
+            last.title = this.data.songList[i].name;
+            last.coverImgUrl = this.data.songList[i].pic;
+          })
+        
+          back.onTimeUpdate(function(){
+            const back = wx.getBackgroundAudioManager();
+              var currentTime =back.currentTime;
+              var percent =Math.floor(100*currentTime/that.data.song.time);
+              // if(percent!=that.data.percent){
+              //   that.setData({
+              //     currentTime: Math.floor(currentTime),
+              //     percent: percent
+              //   })
+              // }else{
+                that.setData({
+                  currentTime: Math.floor(currentTime),
+                  percent: percent
+                })
+              // }
+              // console.log(percent);
+          })
+          back.onPause(()=>{
+            that.setData({
+              playStatu:'play.png',
+              zhuanma:'paused'
+            })
+            // console.log(that.data.playStatu);
+          })
           back.onEnded(() => {
-            console.log("音乐播放结束");
+            var i = Math.floor((Math.random() * that.data.songList.length));
+            const next = wx.getBackgroundAudioManager();
+            that.setData({
+              song: that.data.songList[i]
+            })
+            next.src = that.data.songList[i].url;
+            next.title = that.data.songList[i].name;
+            next.coverImgUrl = that.data.songList[i].pic;
           })
           // console.log(song);
         } else { // 歌词查询出错
@@ -89,6 +237,28 @@ Page({
 
     })
 
+    wx.request({
+      url: 'https://api.bzqll.com/music/netease/search?key=579621905&type=song&limit=100&offset=0&s=' + options.songName,
+      success: res => {
+        // console.log(res.data.code);
+        // console.log(res.data);
+        if (res.data.code == 200) {
+          let searchEnd = res.data.data;
+          // console.log(options.songName);
+          this.setData({
+            songList: res.data.data,
+          })
+          // console.log(this.data.songList[0]);
+        } else {
+          this.setData({
+            songList: [],
+          })
+        }
+      }
+    })
+
+
+    
     
     
 
@@ -105,7 +275,7 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-
+    
   },
 
   /**
--- pages/hots/hots.js
+++ pages/hots/hots.js
@@ -5,16 +5,59 @@ Page({
    * 页面的初始数据
    */
   data: {
+    // 轮播图部分数据
+    imgUrls: [
+      '/image/lunbo1.png',
+      '/image/lunbo2.png',
+      '/image/lunbo3.png'
+    ],
+    hotList:'',
+    theEnd2:true,
+    indicatorDots: false,
+    autoplay: true,
+    interval: 3000,
+    duration: 1000,
     songs:[],
+    keywords:'',
     songListName:'',
     songListDescription:'',
     searchEnd: [], //列表
     theEnd: false  //是否显示搜索结果
   },
+  // 轮播图部分开始
+  changeIndicatorDots(e) {
+    this.setData({
+      indicatorDots: !this.data.indicatorDots
+    })
+  },
+  changeAutoplay(e) {
+    this.setData({
+      autoplay: !this.data.autoplay
+    })
+  },
+  intervalChange(e) {
+    this.setData({
+      interval: e.detail.value
+    })
+  },
+  durationChange(e) {
+    this.setData({
+      duration: e.detail.value
+    })
+  },
+  // 轮播图结束
 
+  searchResult: function () {
+    var keywords = this.data.keywords;
+    // console.log('111'+keywords);
+    wx.navigateTo({
+      url: '../searchResult/searchResult?keywords='+keywords,
+    })
+  },
   searchTap(e) {
     //1.获取关键词
     var keywords = e.detail.value
+    // console.log(keywords);
     //2.判断过滤
     if (!keywords) {
       wx.showToast({
@@ -23,6 +66,9 @@ Page({
         duration: 2000
       })
     }
+    this.setData({
+      keywords:keywords
+    })
     //3.发送请求设置数据
     wx.request({
       url: 'https://api.bzqll.com/music/netease/search?key=579621905&type=song&limit=10&offset=0&s='+ keywords,
@@ -33,12 +79,14 @@ Page({
           let searchEnd = res.data.data;
           this.setData({
             searchEnd: res.data.data,
-            theEnd: true
+            theEnd: true,
+            theEnd2:false
           })
         } else {
           this.setData({
             searchEnd: [],
-            theEnd: false
+            theEnd: false,
+            theEnd2:true
           })
         }
       }
@@ -51,9 +99,23 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    url: 'https://api.bzqll.com/music/netease/topMvList?key=579621905&limit=10&offset=0',
-      // 请求热单
-
+      wx.request({
+        url: 'https://api.bzqll.com/music/netease/highQualitySongList?key=579621905&cat=全部&limit=6',
+        success: res => {
+          // console.log(res.data.data);
+          // console.log(res);
+          if (res.data.code == 200) {
+            let hotList = res.data.data.playlists;
+            this.setData({
+              hotList: hotList
+            })
+          } else {
+            this.setData({
+              hotList: []
+            })
+          }
+        }
+      })
 
       wx.request({
         url:'https://api.bzqll.com/music/netease/songList?key=579621905&id=19723756&limit=10',
